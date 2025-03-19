ed9b0c7e778818168f405e2ff1a4a08a6a3f6bc0
Mizhab K Ansar <mizh48.ansar@gmail.com>
2025-03-19 12:21:23 +0530

    commit

--- ABDMSessionandcertAPI/obj/Debug/net9.0/ABDMSessionandcertAPI.AssemblyInfo.cs
+++ ABDMSessionandcertAPI/obj/Debug/net9.0/ABDMSessionandcertAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABDMSessionandcertAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f57217c9f0889bef71d20d2688d877030514ca52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f9707c8bc61598303c11202187ace28fc56a7d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABDMSessionandcertAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABDMSessionandcertAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
